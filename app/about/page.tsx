de2559b379faa3ab12557dc9b1e335205fc39cd5
satyajittalukder <dorhubs@gmail.com>
2026-01-30 18:11:34 +0530

    feat: enhance styling and layout with Tailwind CSS, add new components and utilities

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="text-5xl underline bg-amber-600 text-center">
+      <h1>About Page</h1>
+    </div>
+  )
+}
+
+export default page
