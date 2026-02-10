56974b60339e09d745556aa683730075be387c7a
satyajittalukder <dorhubs@gmail.com>
2026-02-10 11:50:57 +0530

    root index of the db models

---
+++ lib/models/index.ts
@@ -0,0 +1,7 @@
+import "./board";
+import "./column";
+import "./jobApplication";
+
+export { default as Board } from "./board";
+export { default as Column } from "./column";
+export { default as JobApplication } from "./jobApplication";
